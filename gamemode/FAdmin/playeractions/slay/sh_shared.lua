91ae4b492e1d9b4c712f3e2d90596e2c5c99a05c
Carl Beattie <12beattiecastp@gmail.com>
2022-05-06 13:49:24 +0100

    delete folder, to re-add with correct name

--- gamemode/FAdmin/playeractions/slay/sh_shared.lua
+++
@@ -1,5 +0,0 @@
-FAdmin.PlayerActions.SlayTypes = {}
-FAdmin.PlayerActions.SlayTypes[1] = "Normal"
-FAdmin.PlayerActions.SlayTypes[2] = "Silent"
-FAdmin.PlayerActions.SlayTypes[3] = "Explode"
-FAdmin.PlayerActions.SlayTypes[4] = "Rocket"
