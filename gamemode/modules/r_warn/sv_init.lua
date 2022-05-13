e62be9f7c52e9f80485238d40a15208c70a0780c
carl beattie <12beattiecastp@gmail.com>
2022-05-14 00:19:31 +0100

    fix: r_warn command now checking if chat command is equal to !warn

--- gamemode/modules/r_warn/sv_init.lua
+++ gamemode/modules/r_warn/sv_init.lua
@@ -1,6 +1,12 @@
 util.AddNetworkString("open_r_warn")
 
+local aloudGroups = {
+  "admin"
+}
+
 function sendHWarnMenuOpenMessage(ply)
+
+  print(ply:IsUserGroup("superadmin"))
 
   net.Start("open_r_warn")
   
@@ -8,6 +14,14 @@ function sendHWarnMenuOpenMessage(ply)
 
 end
 
-hook.Add( "PlayerSay" , "!warn", sendHWarnMenuOpenMessage)
+hook.Add( "PlayerSay" , "warn", function(ply, text)
+
+  if (string.lower( text ) == "!warn") then
+
+    sendHWarnMenuOpenMessage(ply)
+  
+  end
+
+end)
 
 concommand.Add("r_warn", sendHWarnMenuOpenMessage)
