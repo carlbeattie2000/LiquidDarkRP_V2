81349c1eb9335df1b766df5466919e3fe17a917e
carl beattie <12beattiecastp@gmail.com>
2022-05-20 02:04:36 +0100

    fix: add warn sql functions

--- gamemode/modules/r_warn/sv_init.lua
+++ gamemode/modules/r_warn/sv_init.lua
@@ -1,18 +1,20 @@
 util.AddNetworkString("open_r_warn")
 
+local meta = FindMetaTable("Player")
+
 local aloudGroups = {
   ["superadmin"] = {"*"},
   ["admin"] = {"warn", "remove_warn"},
   ["moderator"] = {"warn"}
 }
 
-function checkPlayerCanOpen(ply)
+function meta:playerHasPerms()
 
   local aloud = false
 
   for k, v in pairs(aloudGroups) do
 
-    if ply:IsUserGroup(k) then
+    if self:IsUserGroup(k) then
 
       aloud = true
     
@@ -26,7 +28,7 @@ end
 
 function sendHWarnMenuOpenMessage(ply)
 
-  if !checkPlayerCanOpen(ply) then return end
+  if !ply:playerHasPerms() then return end
 
   net.Start("open_r_warn")
   
@@ -47,3 +49,97 @@ hook.Add( "PlayerSay" , "warn", function(ply, text)
 end)
 
 concommand.Add("r_warn", sendHWarnMenuOpenMessage)
+
+function setupSqlTable()
+
+  sql.Query("CREATE TABLE IF NOT EXISTS r_warns (id INTEGER PRIMARY KEY AUTOINCREMENT, steam_id TEXT, reason TEXT, warn_time INTEGER, active INTEGER)")
+
+end
+setupSqlTable()
+
+function newWarnSQL(steam_id, reason)
+
+  local currentTimeMilliseconds = os.time() * 1000
+
+  sql.Query("INSERT INTO r_warns (steam_id, reason, warn_time, active) VALUES (" 
+  .. sql.SQLStr(steam_id) .. "," 
+  .. sql.SQLStr(reason) .. "," 
+  .. sql.SQLStr(currentTimeMilliseconds) .. "," 
+  .. sql.SQLStr(1) 
+  .. ")")
+
+end
+
+function editWarnSQL(warnID, newReason)
+
+  local warnFound = sql.Query("SELECT id FROM r_warns WHERE id = " .. sql.SQLStr(warnID) .. ";")
+
+  if warnFound then
+
+    sql.Query("UPDATE r_warns SET reason = " .. sql.SQLStr(newReason) .. " WHERE id = " .. sql.SQLStr(warnID) .. ";")
+
+    return true
+  
+  end
+
+  return false
+
+end
+
+function findPlayersWarnsSQL(steam_id)
+
+  local warnsFound = sql.Query("SELECT * FROM r_warns WHERE steam_id = " .. sql.SQLStr(steam_id) .. ";")
+
+  if warnsFound then return warnsFound end
+
+  return false
+
+end
+
+function removeWarnSQL(warnID)
+
+  local warnFound = local warnFound = sql.Query("SELECT id FROM r_warns WHERE id = " .. sql.SQLStr(warnID) .. ";")
+
+  if warnFound then
+
+    sql.Query("DELETE FROM r_warns WHERE id = " .. sql.SQLStr(warnID) .. ";")
+
+    return true
+
+  end
+
+  return false
+
+end
+
+function clearPlayersWarnsSQL(steam_id)
+
+  local warnsFound = sql.Query("SELECT id FROM r_warns WHERE steam_id = " .. sql.SQLStr(steam_id) .. ";")
+
+  if warnsFound then
+
+    sql.Query("DELETE FROM r_warns WHERE steam_id = " .. sql.SQLStr(steam_id) .. ";")
+
+    return true
+
+  end
+
+  return false
+
+end
+
+function warnExpiredSQL(warnID)
+
+  local warnFound = local warnFound = sql.Query("SELECT id FROM r_warns WHERE id = " .. sql.SQLStr(warnID) .. ";")
+
+  if warnFound then
+
+    sql.Query("UPDATE r_warns SET active = " .. sql.SQLStr(0) .. " WHERE id = " .. sql.SQLStr(warnID) .. ";")
+
+    return true
+
+  end
+
+  return false
+  
+end
